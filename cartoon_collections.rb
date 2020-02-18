95f946b340d0d3ee31edb3e54a9104f20ca06659
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-18 03:00:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,17 @@ def roll_call_dwarves(array) # code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
+  newArray = []
+  index = 0
+  while index < array.length do
+    newArray << array[index].capitalize + "!"
+    index += 1
+  end
+  newArray
+end
+
 end
 
 def long_planeteer_calls# code an argument here

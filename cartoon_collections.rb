ac9ef7dc15da363b828f835084835b5207f11da2
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-18 02:56:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,12 @@
 def roll_call_dwarves(array) # code an argument here
   # Your code here
   index = 0
+  while index < array.length do
+    puts "#{index + 1}. #{array[index]}"
+    index += 1
+  end
 end
+roll_call_dwarves(array)
 
 def summon_captain_planet# code an argument here
   # Your code here

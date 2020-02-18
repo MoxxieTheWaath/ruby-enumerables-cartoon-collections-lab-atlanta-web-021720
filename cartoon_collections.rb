1ee19407f70c465dd55335b0b520f080324b04ca
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-18 03:04:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,9 @@ def summon_captain_planet(array) # code an argument here
   newArray
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls) # code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
